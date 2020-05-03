c5907f775af226abc61c7c62ede68cc84f4b9b56
jorgeSamurSansur <50024270+jorgeSamurSansur@users.noreply.github.com>
2020-05-02 20:19:32 -0400

    LRU listo?
    agregue lo de LRU deberia estar funcional
    arme una API para el LRU con la LRU en estado global

--- servidor.py
+++ servidor.py
@@ -3,8 +3,30 @@ import socket
 #49152-6535 puertos disponibles
 port = 5004 #Puerto ocupado en TCP
 BUFFER_SIZE = 2048 
+LRU = [ ]
 
+def searchInLRU(domain):
+    c = 0
+    for t in LRU:
+        if domain in t:
+            return c
+        c+=1
 
+    return -1
+
+def giveItToMe(domain):
+    index = searchInLRU(domain)
+    if index != -1:
+        return LRU[index][1]
+    else:
+        return "Error"
+
+def welcomeNewGuy(domain, response):
+    if len(LRU) == 5:
+        LRU.append(tuple((domain, response)))
+        LRU.pop(0)
+    else:
+        LRU.append(tuple((domain, response)))
 
 
 def getHeader(DOMAIN):       
@@ -51,15 +73,29 @@ while True:
             print("# Conexión terminada")                    
             break                             
         else:    
-            existe_en_LRU=0    
-            #existe_en_LRU=BuscarEnLRU(URL)            
+            inLRU=0    
+            inLRU= searchInLRU(URL)           
             print("# Buscando el HEADER de la url "+str(URL))
             header,estado = getHeader(URL)                                            
-            if estado==-1 and existe_en_LRU==0:
+            if estado==-1 and inLRU==-1: # si el url no se encuentra en la web ni en la LRU
                 respuesta="FAIL?|¿5006"
                 TCP_SOCKET_CLIENTE.send(respuesta.encode()) # Hacemos echo convirtiendo de nuevo a bytes       
                 print("# No se encontro el sitio web o no esta disponible")                             
-            else:            
+            
+            elif estado!=-1 and inLRU != -1: #si es q esta en la Web y en nuestra LRU
+                #print(inLRU)
+                header = giveItToMe(URL)
+                respuesta="OK?|¿5006"
+                TCP_SOCKET_CLIENTE.send(respuesta.encode()) # Hacemos echo convirtiendo de nuevo a bytes              
+                print("# Enviado mensaje para conexion UDP")
+                ConexionUDP(header)#deberiamos remplazar el valor por el header                
+                print("# Contenido del HEADER ENVIADO")
+                print("# Regresando a Conexion TCP")    
+
+
+            else: #existe peroooo no la tengo en la LRU
+                welcomeNewGuy(URL, header)
+                #print(URL + "agregado")
                 respuesta="OK?|¿5006"
                 TCP_SOCKET_CLIENTE.send(respuesta.encode()) # Hacemos echo convirtiendo de nuevo a bytes              
                 print("# Enviado mensaje para conexion UDP")
