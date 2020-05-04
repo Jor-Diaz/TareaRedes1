e30fdcc6d223a190de7c67905eb4f246a2509c61
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-05-03 21:45:52 -0400

    ultimos cambios

--- cliente.py
+++ cliente.py
@@ -5,29 +5,23 @@ host='localhost'
 port = 5007 #Puerto ocupado en TCP
 BUFFER_SIZE = 2048
 
-
-def createFile(response,URL):
-	arch=open(str(URL)+".txt","a+")
+def createFile(response,URL):#Creamos el archivo para guardar el header del dominio
+	arch=open(str(URL)+".txt","w")#creamos el archivo
 	arch.write("-------------------------------------------------")
-	arch.write("Contenido Header de la URL: "+str(URL))
+	arch.write("Contenido Header de la URL: "+str(URL)) #Guardamos el contenido
 	arch.write(response)
-	arch.close()
+	arch.write("-------------------------------------------------")
+	arch.close()#cerramos el archivo
 	
-
-
 def ConexionUDP(direccion_servidor,port,URL): #Funcion para la conexion TCP
 	UDP_SOCKET_CLIENTE=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #Creamos el socket UDP		
 	mensaje = "OK"                       
 	UDP_SOCKET_CLIENTE.sendto(mensaje.encode(),(direccion_servidor,int(port))) #Enviamos el mensaje OK		
 	response, _ = UDP_SOCKET_CLIENTE.recvfrom(BUFFER_SIZE) #Obtenemos la respuesta del servidor	
 	UDP_SOCKET_CLIENTE.close()#Cerramos conexion UDP
-	createFile(response.decode(),URL)	
+	createFile(response.decode(),URL)#creamos el archivo	
 	print("# Header Recibido")			
 	return
-
-
-
-
 
 TCP_SOCKET_CLIENTE=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Creamos Conexion TCP
 TCP_SOCKET_CLIENTE.connect((host, port)) #Nos conectamos
@@ -38,16 +32,17 @@ print("#########################################################################
 while mensaje!="terminate": 
 	print("# Ingrese el nombre de la página a buscar o ingrese 'terminate' para cerrar conexión")
 	mensaje=input("Ingrese opción: ") #Pedimos URL
-	TCP_SOCKET_CLIENTE.send(mensaje.encode('utf-8'))
+	TCP_SOCKET_CLIENTE.send(mensaje.encode())
 	if mensaje!='terminate':#verificamos que no se quiera terminar conexion
 		data = TCP_SOCKET_CLIENTE.recv(BUFFER_SIZE)#recibimos resultado
 		data=data.decode()				
 		status,puerto=data.strip().split("?|¿")
-		if status=="OK":
+		if status=="OK":#si existe la pagina
 			ConexionUDP('localhost',puerto,mensaje)#Creamos conexion UDP
-		else:
+		else:#si es que no existe la pagina
 			print("[°](Mensaje Servidor) No se encontro el sitio web o no estaba disponible")	
 	print("-------------------------------------------") 	
-TCP_SOCKET_CLIENTE.close() 	
+print("# Conexion TCP terminada")
+TCP_SOCKET_CLIENTE.close()#terminamos la conexion 	
 
 
--- servidor.py
+++ servidor.py
@@ -7,78 +7,77 @@ LRU = [ ]
 
 
 
-def save_cache_LRU():
-    arch=open("cache.txt","w")
-    for i in LRU:
-        arch.write(i[0])        
+def save_cache_LRU():#Funcion para guardar lo existente en la LRU en un archivo
+    arch=open("cache.txt","w")#Abrimos el archivo Cache
+    for i in LRU:#Escribimos el contenido de la URL
+        arch.write(i[0])#Escribimos el dominio        
         arch.write("\n")
-        arch.write("!!!!\n")        
-        arch.write(i[1])
+        arch.write("!!!!\n")#Escribimos un separador        
+        arch.write(i[1])#Escribimos el header
         arch.write("\n")
-        arch.write("#######\n")
-    arch.close()
+        arch.write("#######\n")#Separador 
+    arch.close()#Cerramos el archivo
 
-def cargar_LRU():
-    arch=open("cache.txt","r")
-    a=0
+def cargar_LRU():#Cargamos la data de la LRU
+    arch=open("cache.txt","r")#Leemos el archivo cache 
+    a=0#variable auxiliar para saber si hemos o no detectado un header
     dominio=""
-    aux=""
-    header=""    
+    aux=""#variable auxiliar para almacenar el dominio 
+    header="" #variable auxiliar para almacenar el header   
     for i in arch:
-        if a==1:
-            if i=="#######\n":                
-                LRU.append(tuple((dominio, header)))
-                header=""
+        if a==1:#ya encontramos un dominio
+            if i=="#######\n":            #Termino el header    
+                LRU.append(tuple((dominio, header)))#Cargamos a la LRU
+                header=""#Seteamos denuevo las variables
                 a=0
             else:
-                header+=i  
-        if a==0:        
-            if i=="!!!!\n":                
-               dominio=aux.strip()               
-               a=1
+                header+=i  #Seguimos guardando header
+        if a==0:  # Aun no encontramos dominio
+            if i=="!!!!\n": #Encontramos dominio               
+               dominio=aux.strip()#almacenamos el dominio               
+               a=1#Marcamos la variable auxiliar
             aux=i            
+    arch.close()#Cerramos el archivo
 
-    arch.close()
-
-cargar_LRU()
-for i in LRU:
-    print(i[0])
-def searchInLRU(domain):   
+def searchInLRU(domain):#Buscamos el dominio en La LRU
     pos=0 
     for t in LRU:
         if domain in t:
-            return pos
+            return pos#Retornamos la posicion si es que esta
         pos+=1        
-    return -1
+    return -1#Retornamos -1 si no esta
 
-def obtenerDeLRU(domain):
-    index = searchInLRU(domain)
-    if index != -1:
+def obtenerDeLRU(domain):#obtenemos el header de un dominio en la LRU
+    index = searchInLRU(domain)#buscamos la posicion
+    if index != -1:#si si esta retornamos el contenido
         return LRU[index][1]
     else:
-        return "Error"
+        return "Error"#sino error por que antes fue verificada
 
-def add_to_LRU(domain, response):
-    if len(LRU) == 5:
-        LRU.append(tuple((domain, response)))
-        LRU.pop(0)
+def add_to_LRU(domain, response):#agregamos un dominio y su contenido a la LRU
+    if len(LRU) == 5:#Largo maximo LRU por enunciado de la tarea
+        LRU.append(tuple((domain, response)))#Agregamos el contenido
+        LRU.pop(0)#sacamos uno
     else:
-        LRU.append(tuple((domain, response)))
+        LRU.append(tuple((domain, response)))#agregamos
 
 
 def getHeader(DOMAIN):       
-    request = "GET / HTTP/1.1\nHost: "+DOMAIN+"\n\n"
-    request = bytes(request, encoding="ascii") ### no borrar
-    socketWeb = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        socketWeb.connect((DOMAIN, 80))
-        socketWeb.send(request)
-    except Exception as e:
+    request = "GET / HTTP/1.1\nHost: "+DOMAIN+"\n\n" #Creamos la peticion http 
+    request = bytes(request, encoding="ascii") # codificamos la peticion
+    socketWeb = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#creamos el socket para conectarnos
+    try:#vemos si existe el sitio
+        socketWeb.connect((DOMAIN, 80))#nos conectamos al dominio en el puerto 80
+        socketWeb.send(request) #hacemos la peticion
+
+    except Exception as e:#si no existe retornamos el -1
         return "",-1        
-    result = socketWeb.recv(2048)    
+    result = socketWeb.recv(2048) #Recibimos la respuesta
+    #hacemos split a al header de diferentes tipos para limpiar el contenido   
     result=result.decode().split("<HTML>")
     result=result[0].split("<html>")
     result=result[0].split("<!DOCTYPE")
+    socketWeb.close()#Cerramos el socket
     print("# Contenido del Header obtenido")            
     return result[0], 0
 
@@ -95,7 +94,7 @@ def ConexionUDP(respuesta):#Funcion para conexion UDP
     return
 
 ## MAIN
-#LRU=[('DOMINIO','RESPUESTA'),('DOMINIO','RESPUESTA'),('DOMINIO','RESPUESTA'),('DOMINIO','RESPUESTA')]
+cargar_LRU()
 TCP_SOCKET_SERVIDOR=socket.socket(socket.AF_INET, socket.SOCK_STREAM)# Creamos un objeto socket tipo TCP
 TCP_SOCKET_SERVIDOR.bind(('', port)) 
 TCP_SOCKET_SERVIDOR.listen(1) # Esperamos la conexión del cliente 
@@ -105,39 +104,39 @@ print('# Conexión establecida con el cliente')
 while True:    
     # Recibimos bytes, convertimos en str
     data = TCP_SOCKET_CLIENTE.recv(BUFFER_SIZE)       
-    URL = data.decode("utf-8")                
+    URL = data.decode()                
     print('# Mensaje recibido de cliente: {}'.format(data.decode('utf-8'))) 
     if URL == "terminate":                    
         print("# Conexión terminada")                    
         break                             
     else:    
         inLRU=0    
-        inLRU= searchInLRU(URL)                                                                            
-        if inLRU!=-1:
+        inLRU= searchInLRU(URL)#recibisamos si esta en la LRU                                                                            
+        if inLRU!=-1:#Si esta en la LRU
             print("# HEADER de la url "+str(URL)+" esta en la LRU")   
-            header = obtenerDeLRU(URL)
+            header = obtenerDeLRU(URL)#obtenemos el header de la LRU
             respuesta="OK?|¿5006"
-            TCP_SOCKET_CLIENTE.send(respuesta.encode()) # Hacemos echo convirtiendo de nuevo a bytes              
+            TCP_SOCKET_CLIENTE.send(respuesta.encode()) # enviamos el ok al clinete
             print("# Enviado mensaje para conexion UDP")
-            ConexionUDP(header)#deberiamos remplazar el valor por el header                
+            ConexionUDP(header)#Comenzamos la conexion udp para enviar el header            
             print("# Contenido del HEADER ENVIADO")
             print("# Regresando a Conexion TCP")    
 
         else:
             print("# Buscando el HEADER de la url "+str(URL))   
-            header,estado = getHeader(URL)
+            header,estado = getHeader(URL)#vemos si existe la pagina web  y buscamos el header
             if estado==-1 : # si el url no se encuentra en la web ni en la LRU
                 respuesta="FAIL?|¿5006"
                 TCP_SOCKET_CLIENTE.send(respuesta.encode()) # Hacemos echo convirtiendo de nuevo a bytes       
                 print("# No se encontro el sitio web o no esta disponible")                                                             
-            else: #existe peroooo no la tengo en la LRU
-                add_to_LRU(URL, header)                    
+            else: #existe peroo no la tengo en la LRU
+                add_to_LRU(URL, header)#lo agregamos a la LRU                    
                 respuesta="OK?|¿5006"                    
-                TCP_SOCKET_CLIENTE.send(respuesta.encode()) # Hacemos echo convirtiendo de nuevo a bytes              
+                TCP_SOCKET_CLIENTE.send(respuesta.encode()) # Enviamos el ok al cliente para la conexion UDP
                 print("# Enviado mensaje para conexion UDP")
-                ConexionUDP(header)#deberiamos remplazar el valor por el header                
+                ConexionUDP(header)#Comenzamos la conexion udp para enviar el header                
                 print("# Contenido del HEADER ENVIADO")
                 print("# Regresando a Conexion TCP")                                                         
     print("-------------------------------------------")                                                             
-save_cache_LRU()
-TCP_SOCKET_CLIENTE.close() 
+save_cache_LRU()#Salvamos la LRU en el archivo cache
+TCP_SOCKET_CLIENTE.close()#terminamos la conexion TCP
